0a577e1d7b5af59b615b8eed104e165f821192b7
doctorspider42 <22765089+doctorspider42@users.noreply.github.com>
2026-08-20 16:37:41 +0200

    fix: hide console window for desktop app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "app/Application.h"
 
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    fam::Application app;
+    return app.Run();
+}
+#else
 int main(int, char**) {
     fam::Application app;
     return app.Run();
 }
+#endif
